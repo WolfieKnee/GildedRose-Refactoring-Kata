532d2b72572bf7b9d76e66cf9fe94f9dcb263d09
WolfieKnee <david@davidwolfson.co.uk>
2024-10-07 20:15:33 +0100

    cleaning up comments from previous merge

--- go/gildedrose/gildedrose.go
+++ go/gildedrose/gildedrose.go
@@ -9,25 +9,21 @@ func UpdateItem(item *Item) {
 	if item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert" {
 		if item.Quality > 0 {
 			if item.Name != "Sulfuras, Hand of Ragnaros" {
-				// item.Quality = item.Quality - 1
 				item.Quality -= 1
 
 			}
 		}
 	} else {
 		if item.Quality < 50 {
-			// item.Quality = item.Quality + 1
 			item.Quality += 1
 			if item.Name == "Backstage passes to a TAFKAL80ETC concert" {
 				if item.SellIn < 11 {
 					if item.Quality < 50 {
-						// item.Quality = item.Quality + 1
 						item.Quality += 1
 					}
 				}
 				if item.SellIn < 6 {
 					if item.Quality < 50 {
-						// item.Quality = item.Quality + 1
 						item.Quality += 1
 					}
 				}
@@ -36,7 +32,6 @@ func UpdateItem(item *Item) {
 	}
 
 	if item.Name != "Sulfuras, Hand of Ragnaros" {
-		// item.SellIn = item.SellIn - 1
 		item.SellIn -= 1
 	}
 
@@ -45,7 +40,6 @@ func UpdateItem(item *Item) {
 			if item.Name != "Backstage passes to a TAFKAL80ETC concert" {
 				if item.Quality > 0 {
 					if item.Name != "Sulfuras, Hand of Ragnaros" {
-						// item.Quality = item.Quality - 1
 						item.Quality -= 1
 					}
 				}
@@ -54,7 +48,6 @@ func UpdateItem(item *Item) {
 			}
 		} else {
 			if item.Quality < 50 {
-				// item.Quality = item.Quality + 1
 				item.Quality += 1
 			}
 		}
