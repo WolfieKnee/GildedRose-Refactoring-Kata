acfa21bd5005339224a511caa5238187dcb797a0
WolfieKnee <david@davidwolfson.co.uk>
2024-10-07 20:27:05 +0100

    abstract incrementItemQuality and incrementItemSellIn

--- go/gildedrose/gildedrose.go
+++ go/gildedrose/gildedrose.go
@@ -5,14 +5,26 @@ type Item struct {
 	SellIn, Quality int
 }
 
-func UpdateItem(item *Item) {
-	if item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert" {
-		if item.Quality > 0 {
-			if item.Name != "Sulfuras, Hand of Ragnaros" {
-				item.Quality -= 1
-
-			}
+func (item *Item) incrementItemQuality() {
+	if item.Quality > 0 {
+		if item.Name != "Sulfuras, Hand of Ragnaros" {
+			item.Quality -= 1
 		}
+	}
+}
+
+func (item *Item) incrementItemSellIn() {
+	if item.Name != "Sulfuras, Hand of Ragnaros" {
+		item.SellIn -= 1
+	}
+
+}
+
+func UpdateItem(item *Item) {
+	item.incrementItemSellIn()
+
+	if item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert" {
+		item.incrementItemQuality()
 	} else {
 		if item.Quality < 50 {
 			item.Quality += 1
@@ -31,18 +43,10 @@ func UpdateItem(item *Item) {
 		}
 	}
 
-	if item.Name != "Sulfuras, Hand of Ragnaros" {
-		item.SellIn -= 1
-	}
-
 	if item.SellIn < 0 {
 		if item.Name != "Aged Brie" {
 			if item.Name != "Backstage passes to a TAFKAL80ETC concert" {
-				if item.Quality > 0 {
-					if item.Name != "Sulfuras, Hand of Ragnaros" {
-						item.Quality -= 1
-					}
-				}
+				item.incrementItemQuality()
 			} else {
 				item.Quality = item.Quality - item.Quality
 			}
