c83d42336a1d377d6015b6ee5cefab3346e08852
WolfieKnee <david@davidwolfson.co.uk>
2024-10-08 15:09:03 +0100

    seperate brie and passes logic in quality updates

--- go/gildedrose/gildedrose.go
+++ go/gildedrose/gildedrose.go
@@ -1,14 +1,21 @@
 package gildedrose
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
-//  Type definitions
+// Type definitions
 type Item struct {
 	Name            string
 	SellIn, Quality int
 }
 
 type Sulfuras struct {
+	Item
+}
+
+type Passes struct {
 	Item
 }
 
@@ -36,8 +43,27 @@ func (item *Item) updateItem() {
 	}
 }
 
+// Methods for Sulfuras
 // note - These do nothing, may be removed later
-func (SulfurasItemitem *Sulfuras) updateItem() {}
+func (SulfurasItem *Sulfuras) updateItem() {}
+
+// Methods for Passes
+func (PassesItem *Passes) updateItem() {
+	fmt.Print("hello from passes \n")
+}
+
+func (PassesItem *Passes) UpdatePassQuality() {
+	if PassesItem.SellIn < 11 {
+		if PassesItem.Quality < maxQual {
+			PassesItem.incrementItemQuality()
+		}
+	}
+	if PassesItem.SellIn < 6 {
+		if PassesItem.Quality < maxQual {
+			PassesItem.incrementItemQuality()
+		}
+	}
+}
 
 // magic numbers
 var maxQual int = 50
@@ -46,10 +72,17 @@ var minQual int = 0
 // functions
 
 func BaselineUpdateItem(item *Item) {
-	if item.Name == "Aged Brie" || item.Name == "Backstage passes to a TAFKAL80ETC concert" {
+
+	// update the quality
+	if item.Name == "Aged Brie" {
+		if item.Quality < maxQual {
+			item.Quality += 1
+		}
+	} else if item.Name == "Backstage passes to a TAFKAL80ETC concert" {
 		if item.Quality < maxQual {
 			item.Quality += 1
 			if item.Name == "Backstage passes to a TAFKAL80ETC concert" {
+
 				if item.SellIn < 11 {
 					if item.Quality < maxQual {
 						item.incrementItemQuality()
@@ -62,13 +95,18 @@ func BaselineUpdateItem(item *Item) {
 				}
 			}
 		}
+
 	} else {
 		item.decrementItemQuality()
 	}
+
+	// decrement the sell in
 	item.decrementItemSellIn()
 
+	// behaviours for SellIn < 0
 	if item.SellIn < 0 {
 		if item.Name != "Aged Brie" {
+			// set Q for expired tickets to zero
 			if item.Name == "Backstage passes to a TAFKAL80ETC concert" {
 				item.Quality = item.Quality - item.Quality
 			} else {
@@ -87,6 +125,10 @@ func UpdateQuality(items []*Item) {
 		if strings.Contains(item.Name, "Sulfuras") {
 			SulfurasItem := Sulfuras{Item: *item}
 			SulfurasItem.updateItem()
+		} else if strings.Contains(item.Name, "Backstage passes") {
+			PassesItem := Passes{Item: *item}
+			PassesItem.updateItem()
+			BaselineUpdateItem(item)
 		} else {
 			BaselineUpdateItem(item)
 		}
