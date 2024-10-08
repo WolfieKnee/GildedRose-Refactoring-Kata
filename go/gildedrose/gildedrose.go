98b31e60802e02f4baf74de1728d6ffa07ceb66c
WolfieKnee <david@davidwolfson.co.uk>
2024-10-08 11:24:54 +0100

    max and min qaulity as global vars

--- go/gildedrose/gildedrose.go
+++ go/gildedrose/gildedrose.go
@@ -13,7 +13,7 @@ type Sulfuras struct {
 // Methods for Item
 
 func (item *Item) incrementItemQuality() {
-	if item.Quality > 0 {
+	if item.Quality > minQual {
 		item.Quality -= 1
 	}
 }
@@ -35,6 +35,10 @@ func (item *Item) incrementItemSellIn() {
 func (SulfurasItemitem *Sulfuras) incrementItemQuality() {}
 func (SulfurasItemitem *Sulfuras) incrementItemSellIn()  {}
 
+// magic numbers
+var maxQual int = 50
+var minQual int = 0
+
 // functions
 
 func UpdateItem(item *Item) {
@@ -52,16 +56,16 @@ func UpdateItem(item *Item) {
 		if item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert" {
 			item.incrementItemQuality()
 		} else {
-			if item.Quality < 50 {
+			if item.Quality < maxQual {
 				item.Quality += 1
 				if item.Name == "Backstage passes to a TAFKAL80ETC concert" {
 					if item.SellIn < 11 {
-						if item.Quality < 50 {
+						if item.Quality < maxQual {
 							item.Quality += 1
 						}
 					}
 					if item.SellIn < 6 {
-						if item.Quality < 50 {
+						if item.Quality < maxQual {
 							item.Quality += 1
 						}
 					}
@@ -70,7 +74,7 @@ func UpdateItem(item *Item) {
 		}
 		item.incrementItemSellIn()
 
-		if item.SellIn < 0 {
+		if item.SellIn < minQual {
 			if item.Name != "Aged Brie" {
 				if item.Name != "Backstage passes to a TAFKAL80ETC concert" {
 					item.incrementItemQuality()
@@ -78,7 +82,7 @@ func UpdateItem(item *Item) {
 					item.Quality = item.Quality - item.Quality
 				}
 			} else {
-				if item.Quality < 50 {
+				if item.Quality < maxQual {
 					item.Quality += 1
 				}
 			}
