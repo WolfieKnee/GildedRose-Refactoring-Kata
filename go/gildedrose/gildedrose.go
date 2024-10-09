73132983f025282be28c4619a9d720d7d24d3dde
WolfieKnee <david@davidwolfson.co.uk>
2024-10-09 09:30:19 +0100

    restructure for clarity

--- go/gildedrose/gildedrose.go
+++ go/gildedrose/gildedrose.go
@@ -4,6 +4,9 @@ import (
 	"strings"
 )
 
+var MAXQUAL int = 50
+var MINQUAL int = 0
+
 // Type definitions
 type Item struct {
 	Name            string
@@ -22,7 +25,11 @@ type Brie struct {
 	*Item
 }
 
-// Methods for Item
+type ItemWrapper interface {
+	updateItem(item *Item)
+}
+
+// Utility Methods
 func (item *Item) decrementItemQuality() {
 	if item.Quality > MINQUAL {
 		item.Quality -= 1
@@ -39,16 +46,24 @@ func (item *Item) decrementItemSellIn() {
 	item.SellIn -= 1
 }
 
+// Item updaters
 func (item *Item) updateItem() {
 	item.decrementItemSellIn()
 	item.decrementItemQuality()
 }
 
-// Methods for Sulfuras
 // note - These do nothing, may be removed later
 func (SulfurasItem *Sulfuras) updateItem() {}
 
-// Methods for Passes
+func (PassesItem *Brie) updateItem() {
+	PassesItem.incrementItemQuality()
+	PassesItem.decrementItemSellIn()
+	if PassesItem.SellIn < 0 {
+		PassesItem.incrementItemQuality()
+	}
+
+}
+
 func (PassesItem *Passes) updateItem() {
 	PassesItem.incrementItemQuality()
 	if PassesItem.SellIn <= 10 {
@@ -65,27 +80,14 @@ func (PassesItem *Passes) updateItem() {
 
 }
 
-// Method for brie
-func (PassesItem *Brie) updateItem() {
-	PassesItem.incrementItemQuality()
-	PassesItem.decrementItemSellIn()
-	if PassesItem.SellIn < 0 {
-		PassesItem.incrementItemQuality()
-	}
-
-}
-
-// magic numbers
-var MAXQUAL int = 50
-var MINQUAL int = 0
-
 func UpdateQuality(items []*Item) {
 	for _, item := range items {
-
+		var wrappedItem ItemWrapper
 		switch {
 		case strings.Contains(item.Name, "Sulfuras"):
-			SulfurasItem := Sulfuras{item}
-			SulfurasItem.updateItem()
+			wrappedItem = Sulfuras{item}
+			// SulfurasItem := Sulfuras{item}
+			// SulfurasItem.updateItem()
 		case strings.Contains(item.Name, "Backstage passes"):
 			PassesItem := Passes{item}
 			PassesItem.updateItem()
@@ -95,6 +97,7 @@ func UpdateQuality(items []*Item) {
 		default:
 			item.updateItem()
 		}
+		wrappedItem.updateItem()
 
 	}
 }
